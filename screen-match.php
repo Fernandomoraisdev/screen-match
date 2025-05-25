66c4e93657a5240076f7b79e6e718edce592af9e
fernandomoraisdev <fernandomoraisdev@gmail.com>
2025-05-25 19:21:54 -0300

    funcao adicionada

--- screen-match.php
+++ screen-match.php
@@ -1,5 +1,15 @@
 <?php
 
+function exibeMensagemLancamento ($ano) {
+    if ($ano > 2022){
+        echo "Esse filme e um lancamento" . PHP_EOL;
+    }elseif($ano > 2020 && $ano <= 2022){
+        echo "Esse filme ainda e novo" . PHP_EOL;
+    }else{
+        echo "Esse filme nao e um lancamento" . PHP_EOL;
+    }
+}
+
 echo "Bem vindo(a) ao screen-metch" .  PHP_EOL;
 
 $nomeFilme = "Top Gun - Maverick";
@@ -22,13 +32,7 @@ echo "Nome do Filme: " . $nomeFilme . PHP_EOL;
 echo "Nota do Filme: " . $notaFilme . PHP_EOL;
 echo "Ano de Lancamento: " . $anoLancamento . PHP_EOL;
 
-if ($anoLancamento > 2022){
-    echo "Esse filme e um lancamento" . PHP_EOL;
-}elseif($anoLancamento > 2020 && $anoLancamento <= 2022){
-    echo "Esse filme ainda e novo" . PHP_EOL;
-}else{
-    echo "Esse filme nao e um lancamento" . PHP_EOL;
-}
+exibeMensagemLancamento($anoLancamento);
 
 $genero = match ($nomeFilme){
     "Top Gun - Maverick" => "acao",
