3686f8ca55d39b2699c6c15ac49ac0fa13d528ed
fernandomoraisdev <fernandomoraisdev@gmail.com>
2025-05-26 19:09:06 -0300

    funcao adicionada

--- screen-match.php
+++ screen-match.php
@@ -1,20 +1,5 @@
 <?php
-
-// Função que exibe uma mensagem sobre o status de lançamento do filme baseado no ano
-function exibeMensagemLancamento(int $ano): void { //diz que esse codigo nao retorna nada
-    if ($ano > 2022) {
-        echo "Esse filme e um lancamento" . PHP_EOL; // Se lançado após 2022
-    } elseif ($ano > 2020 && $ano <= 2022) {
-        echo "Esse filme ainda e novo" . PHP_EOL; // Se lançado entre 2021-2022
-    } else {
-        echo "Esse filme nao e um lancamento" . PHP_EOL; // Se lançado em 2020 ou antes
-    }
-}
-
-function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
-    return $planoPrime || $anoLancamento < 2020;
-}
-
+require_once "funcoes.php";
 // Mensagem de boas-vindas ao sistema
 echo "Bem vindo(a) ao screen-match" . PHP_EOL;
 
