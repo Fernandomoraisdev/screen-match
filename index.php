064d0397c3290554c15a7d0fa03fd6dc02f5c56e
fernandomoraisdev <fernandomoraisdev@gmail.com>
2025-06-03 14:04:04 -0300

    Mais methodos adicionados

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require __DIR__ . "/src/Modelo/Genero.php";
 require __DIR__ . "/src/Modelo/Titulo.php";
 require __DIR__ . "/src/Modelo/Serie.php";
 require __DIR__ . "/src/Modelo/Filme.php";
+require __DIR__ . "/src/Calculos/CalculadoraDeMaratona.php";
 
 echo "Bem-Vindo ao Screen-Match\n";
 
@@ -35,3 +36,10 @@ echo $serie->anoLancamento . "\n";
 $serie->avalia(8);
 
 echo $serie->media() . "\n";
+
+$calculadora =new CalculadoraDeMaratona();
+$calculadora->inclui($filme);
+$calculadora->inclui($serie);
+$duracao = $calculadora->duracao ();
+
+echo "Para essa maratona, voce precisa de $duracao minutos";
